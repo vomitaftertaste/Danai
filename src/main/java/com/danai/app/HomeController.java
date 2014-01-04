49af609ffd2b364eaab1ee8109690e8c998e806e
vomitaftertaste <haidar.muhammad@gmail.com>
2014-01-04 14:27:29 +0700

    add .project

--- src/main/java/com/danai/app/HomeController.java
+++ src/main/java/com/danai/app/HomeController.java
@@ -6,10 +6,13 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.danai.repository.UserRepo;
 
 /**
  * Handles requests for the application home page.
@@ -18,6 +21,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@Autowired
+	private UserRepo userRepo;
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
@@ -35,5 +41,12 @@ public class HomeController {
 		
 		return "home";
 	}
+	
+	@RequestMapping(value="/user", method=RequestMethod.GET )
+	public String viewUser(Model model) {
+		model.addAttribute("userList", userRepo.getAllUser());
+		return "user";
+	}
+	
 	
 }
